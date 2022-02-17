6a3b17bd1cb23357f9a5238566dd7ef4523f22e6
kayeon <kayeon8855@gmail.com>
2022-02-17 17:16:45 +0900

    update backend cors

--- back/app.js
+++ back/app.js
@@ -41,7 +41,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
     cors({
-        origin: ["http://localhost:3060", "nodebird.com"],
+        origin: ["http://localhost:3060", "nodebird.com", "http://3.34.3.171"],
         credentials: true,
     })
 );
