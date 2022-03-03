e7da5dc19e7fffefcdaee138be1c7851108b3fd7
kayeon <kayeon8855@gmail.com>
2022-03-03 16:39:26 +0900

    check finished

--- back/routes/post.js
+++ back/routes/post.js
@@ -310,7 +310,6 @@ router.get("/:postId", async (req, res, next) => {
                 },
             ],
         });
-        console.log(fullPost);
         res.status(200).json(fullPost);
     } catch (error) {
         console.error(error);
