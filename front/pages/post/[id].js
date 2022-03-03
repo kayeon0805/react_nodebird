9132c7dbc8368faa79b3643fe59c6a096d6e1c87
kayeon <kayeon8855@gmail.com>
2022-03-03 16:03:22 +0900

    update og url

--- front/pages/post/[id].js
+++ front/pages/post/[id].js
@@ -43,12 +43,12 @@ const Post = () => {
                     content={
                         singlePost.Images[0]
                             ? singlePost.Images[0].src
-                            : "https://nodebird.com/favicon.ico"
+                            : "https://nodebird.site/favicon.ico"
                     }
                 />
                 <meta
                     property="og:url"
-                    content={`https://nodebird.com/post/${id}`}
+                    content={`https://nodebird.site/post/${id}`}
                 />
             </Head>
             <PostCard post={singlePost} />
