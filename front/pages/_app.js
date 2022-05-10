fde4f863902e8f0c8ea3a406945a56133c5edc33
kayeon <kayeon8855@gmail.com>
2022-05-10 22:48:20 +0900

    react-slick css 추가

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "antd/dist/antd.min.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Head from "next/head";
 import wrapper from "../store/configureStore";
 
