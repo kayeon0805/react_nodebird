6eff3673bf8524aecda36d8ac413348161c16521
kayeon <kayeon8855@gmail.com>
2022-05-19 16:27:19 +0900

    post -> get

--- front/components/ShowImages.jsx
+++ front/components/ShowImages.jsx
@@ -6,7 +6,7 @@ import { backUrl } from "../config/config";
 const ShowImages = ({ image, postId, removeModifyImagePaths }) => {
     const onDeleteImage = useCallback(async () => {
         await axios
-            .post(`${backUrl}/post/image/${postId}?image=${image}`)
+            .get(`${backUrl}/post/image/${postId}?image=${image}`)
             .then((response) => {
                 if (response) {
                     // 이미 게시글에 추가된 이미지를 삭제할 경우
